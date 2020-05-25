25ed9234af93e81dc53e85a0cce978ab775948b2
root <root@readingtohabit.develop.jp>
2020-05-26 00:01:07 +0900

    [modify] views/register_user/form readingtohabit.css
    register_user_form_topのCSSおよびコンポーネントの内容の直書き

---
+++ readingtohabit/resources/views/register_user/form.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.default')
+
+@section('title', 'Readingtohabitユーザー登録情報入力')
+
+@section('content')
+<div class="register_user_content">
+    <div class="register_user_form_top">
+        <div class="register_user_phase_area">
+            <div class="register_user_phase_content">
+                <div class="phase">登録情報入力</div>
+                <div class="register_user_in_phase"></div>
+            </div>
+            <div class="register_user_phase_content">
+                <div class="phase">登録情報確認</div>
+                <div class="register_user_default"></div>
+            </div>
+            <div class="register_user_phase_content">
+                <div class="phase">登録完了</div>
+                <div class="register_user_default"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="register_user_form">
+        <form action="register_user_check" method="post">
+            @empty ($errors)
+                @php
+                $errors = array();
+                @endphp
+            @endempty
+            {{ csrf_field() }}
+            @include('components.register_user_form_input', ['input' => 'name', 'register_user_info' => $register_user_info, 'errors' => $errors])
+            @include('components.register_user_form_input', ['input' => 'email', 'register_user_info' => $register_user_info, 'errors' => $errors])
+            @include('components.register_user_form_input', ['input' => 'password', 'register_user_info' => $register_user_info, 'errors' => $errors])
+            @include('components.register_user_form_input', ['input' => 'password_to_check', 'register_user_info' => $register_user_info, 'errors' => $errors])
+
+            <div class="caution">
+                登録することによって、<a href="rules">利用規約</a>・<a href="plivacy_policy">プライバシーポリシー</a>に同意しているものとみなします。
+            </div>
+        
+            <div class="submit_btn">
+                <input type = "submit" class="btn_primary_more_than_4_chars" value="登録情報確認画面へ">
+            </div>
+
+            <div class="to_login">
+                ログインは<a href="login">こちら</a>から
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
