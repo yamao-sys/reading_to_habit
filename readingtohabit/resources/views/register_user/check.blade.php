39a5ae05357557edd91f819bc8e5cc889eac8233
root <root@readingtohabit.develop.jp>
2020-05-26 04:03:12 +0900

    [modify] views/register_user/form,check,finish readingtohabit.css
    ・register_user_form_topを除去
    ・content_titleを追加
    ・CSSからregister_user_form_top関連を除去
    ・CSSへ.content_titleを追加

--- readingtohabit/resources/views/register_user/check.blade.php
+++ readingtohabit/resources/views/register_user/check.blade.php
@@ -4,23 +4,7 @@
 
 @section('content')
 <div class="register_user_content">
-    <div class="register_user_form_top">
-        <div class="register_user_phase_area">
-            <div class="register_user_phase_content">
-                <div class="phase">登録情報入力</div>
-                <div class="register_user_default"></div>
-            </div>
-            <div class="register_user_phase_content">
-                <div class="phase">登録情報確認</div>
-                <div class="register_user_in_phase"></div>
-            </div>
-            <div class="register_user_phase_content">
-                <div class="phase">登録完了</div>
-                <div class="register_user_default"></div>
-            </div>
-        </div>
-    </div>
-
+    <div class="content_title">ユーザー登録情報確認</div>
     <div class="register_user_check_area">
         <form action="register_user_do" method="post">
         {{ csrf_field() }}
---
+++ readingtohabit/resources/views/register_user/finish.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('title', 'Readingtohabitユーザー登録完了確認')
+
+@section('content')
+<div class="content">
+    <div class="content_title">ユーザー登録完了</div>
+    <div>
+        <div class="mb_3">
+            Readingtohabitにユーザー登録をしていただき、誠にありがとうございます。<br>
+            ご登録いただいたメールアドレス宛てに「ユーザー登録完了」のメールを送信しました。
+        </div>
+        <div class="mb_3">
+            ログインは<a href="login">こちら</a>から
+        </div>
+        <div class="mb_3">
+            ※もし、メールが届いていない場合は<a href="resend_mail_form">こちら</a>から
+        </div>
+    </div>
+</div>
+@endsection
