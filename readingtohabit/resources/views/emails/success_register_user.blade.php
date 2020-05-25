2e1ab42547b39f64c805d8819722e7d8455d1d14
root <root@readingtohabit.develop.jp>
2020-05-26 06:28:51 +0900

    [modify] ユーザー登録完了メールのクラスのインデント、テンプレートの修正

---
+++ readingtohabit/resources/views/emails/success_register_user.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+<div>
+    こんにちは、Readingtohabit事務局です。
+</div>
+<br>
+<div>
+この度はReadingtohabitにご登録いただき、誠にありがとうございます。<br>
+読書をより有意義にできるようにしていきましょう!
+</div>
+<br>
+<div>
+----------------------<br>
+[ご登録内容]<br>
+・ユーザー名<br>
+&emsp;{{ $name }}<br><br>
+・メールアドレス<br>
+&emsp;{{ $email }}<br>
+----------------------
+</div>
+<br>
+<div>
+ログインは下記のURLから<br>
+<a href="readingtohabit.develop.jp/login">http://readingtohabit.develop.jp/login</a>
+</div>
+</body>
+</html>
