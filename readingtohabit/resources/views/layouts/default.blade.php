4e9286ec2255c65991d8b4c349d5fdedf7e342b7
root <root@readingtohabit.develop.jp>
2020-05-26 01:12:22 +0900

    [modify] readingtohabit.css views/layouts/default views/register_user/form
    ヘッダーのハンバーガーメニュー化

---
+++ readingtohabit/resources/views/layouts/default.blade.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <!-- todo .jsを追加-->
+        <!-- todo .cssを追加-->
+        <link rel="stylesheet" type="text/css" href="css/readingtohabit.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    </head>
+    <body>
+        <div id="wrapper">
+        @isset ($is_login)
+        <header class="header_after_login">
+            <div class="header_after_login_body">
+                <div class="header_after_login_logo">
+                    Readingtohabit
+                </div>
+                <div class="header_navi">
+                    <div class="header_navi_after_login_username">
+                        @isset ($login_username)
+                        {{ $login_username }}さん
+                        @else
+                        nonameさん
+                        @endif
+                    </div>
+                    <div class="header_navi_logout">
+                        <a href="logout">ログアウト</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        @else
+        <header class="header_before_login">
+            <div class="header_before_login_service">
+                <div class="service_area">
+                    <small class="service">読書記録×習慣化サービス</small>
+                </div>
+            </div>
+            <div class="header_before_login_body">
+                <div class="header_body_area">
+                    <div class="header_before_login_logo"><a href="top" class="link_dark">Readingtohabit</a></div>
+                    <div class="header_navi">
+                        <input id="nav_input" type="checkbox" class="nav_unshown">
+                        <label id="nav_open" for="nav_input"><span></span></label>
+                        <label class="nav_unshown" id="nav_close" for="nav_input"></label>
+                        <div id="nav_content">
+                            <div class="mt_2rem mb_2rem">
+                                <a href="register_user_form" class="menu_list">ユーザー登録</a>
+                            </div>
+                            <div class="mt_2rem mb_2rem">
+                                <a href="login" class="menu_list">ログイン</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        @endisset
+
+        <div class="content_area">
+        @yield('content')
+        </div>
+
+        @isset ($is_top_page)
+        <footer class="footer_top_page">
+            <div class="footer_body">
+                <div class="footer_content">
+                    <a href="rules" class="rules_top_page">利用規約</a>
+                    <a href="privacy_policy" class="privacy_policy_top_page">プライバシーポリシー</a>
+                    <a href="specfied_commercial_transaction_law" class="specified_commercial_transaction_law_top_page">特定商取引法表記</a>
+                </div>
+                <div class="footer_copyright_top_page">
+                    copyrights c 2020 KEITA YAMAUCHI ALL RIGHTS RESERVED.
+                </div>
+            </div>
+        </footer>
+        @else
+        <footer class="footer_default">
+            <div class="footer_body">
+                <div class="footer_content">
+                    <a href="rules" class="rules">利用規約</a>
+                    <a href="privacy_policy" class="privacy_policy">プライバシーポリシー</a>
+                    <a href="specfied_commercial_transaction_law" class="specified_commercial_transaction_law">特定商取引法表記</a>
+                </div>
+                <div class="footer_copyright">
+                    copyrights c 2020 KEITA YAMAUCHI ALL RIGHTS RESERVED.
+                </div>
+            </div>
+        </footer>
+        @endif
+
+        <script type="text/javascript" src="js/readingtohabit.js" defer></script>
+        </div>
+    </body>
+</html>
