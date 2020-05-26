f759aa8432b5b61e9d5aa9b7b77936769c29a0e8
root <root@readingtohabit.develop.jp>
2020-05-27 03:33:59 +0900

    [add] views/common/invalid

---
+++ readingtohabit/resources/views/layouts/error.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="css/readingtohabit.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div class="content_area">
+            @yield('content')
+            </div>
+        </div>
+        
+        <script type="text/javascript" src="js/readingtohabit.js" defer></script>
+    </body>
+</html>
