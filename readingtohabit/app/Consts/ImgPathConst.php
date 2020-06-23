3a1c585fe3128b31b1789767cf5c5dd00bcdaf4f
yamao <yamaopanku@gmail.com>
2020-06-23 14:38:57 +0000

    [modify]AddArticleDoTest

--- readingtohabit/app/Consts/ImgPathConst.php
+++ readingtohabit/app/Consts/ImgPathConst.php
@@ -4,7 +4,7 @@ namespace App\Consts;
 
 class ImgPathConst
 {
-    const IMG_ABSOLUTE_PATH = '/var/www/html/readingtohabit/readingtohabit/public/img/';
+    const IMG_ABSOLUTE_PATH = '/var/www/html/readingtohabit_project/readingtohabit/public/img/';
     const IMG_PATH = 'img/';
     const NOIMG_PATH = 'img/noimage.png';
 }
