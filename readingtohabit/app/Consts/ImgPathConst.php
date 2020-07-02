84106f865c7cfb4e3bd49e1c629543e5251806e1
yamao <yamaopanku@gmail.com>
2020-07-03 01:33:55 +0900

    [modify]ios input btn

--- readingtohabit/app/Consts/ImgPathConst.php
+++ readingtohabit/app/Consts/ImgPathConst.php
@@ -4,8 +4,8 @@ namespace App\Consts;
 
 class ImgPathConst
 {
-    const MAIN_VISUAL_IMG_PATH = 'https://readingtohabit-staging-backet.s3-ap-northeast-1.amazonaws.com/main_visual.jpg';
+    const MAIN_VISUAL_IMG_PATH = 'https://readinghabit-production.s3-ap-northeast-1.amazonaws.com/main_visual.jpg';
 
     const IMG_ABSOLUTE_PATH = '/var/www/html/readingtohabit_project/readingtohabit/public/img/';
-    const NOIMG_PATH = 'https://readingtohabit-staging-backet.s3-ap-northeast-1.amazonaws.com/noimage.jpg';
+    const NOIMG_PATH = 'https://readinghabit-production.s3-ap-northeast-1.amazonaws.com/noimage.jpg';
 }
